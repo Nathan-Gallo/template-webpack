a364b29887326a31828bf39a5c8ccfd0fb683678
Gallo, Nathan M (HIMCO, FIN) <nathan.gallo@thehartford.com>
2021-03-10 12:22:40 -0500

    init commit

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,10 @@
 <script>
+	import Library from './library/Library.svelte'
 	export let name;
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<Library />
 </main>
 
 <style>
